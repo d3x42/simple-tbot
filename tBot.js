5026a059e56aa9a851637c64cce0b0da8bd63f67
Stanislav Germanovskii <stanislav.germanovskii@jetbrains.com>
2022-05-25 22:23:29 +0300

    add btn

--- tBot.js
+++ tBot.js
@@ -4,3 +4,6 @@ const initData = Telegram.WebApp.initData || '';
 const initDataUnsafe = Telegram.WebApp.initDataUnsafe || {};
 
 document.querySelector('#greeting').innerHTML = `Hi, ${initDataUnsafe.user.first_name}!`;
+document.querySelector('#command').addEventListener('click', () => {
+  Telegram.WebApp.sendData('!!!')
+})
